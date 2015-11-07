34f60d358be9562910b4c992885eca57f0c7cde1
lazicdusan <lazicdusan@gmail.com>
2015-11-07 22:05:32 +0100

    working?

--- testFirst.py
+++ testFirst.py
@@ -1,5 +1,3 @@
 #test for github
-#commit test another line
 #github instructions website
 https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
-#yet another line in the text
