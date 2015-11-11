98e88a1063718495986e6dbb608de23b2ded6290
lazicdusan <lazicdusan@gmail.com>
2015-11-11 21:31:22 +0100

    fixed the indentations

--- arnold_subdivisionControl.py
+++ arnold_subdivisionControl.py
@@ -1,14 +1,10 @@
 import pymel.core as pm
-
 #select the top geo_C_001_GRUP and run this python to get all the subobjects to catclark 1:
 for mesh in pm.listRelatives(pm.selected(),c=1,ad=1,typ='shape'):
-mesh.aiSubdivType.set(1)
-mesh.aiSubdivIterations.set(1)
+	mesh.aiSubdivType.set(1)
+	mesh.aiSubdivIterations.set(1)
 
 #select the top geo_C_001_GRUP and run this python to get all the subobjects to catclark 2:
-
-import pymel.core as pm
-
 for mesh in pm.listRelatives(pm.selected(),c=1,ad=1,typ='shape'):
-mesh.aiSubdivType.set(1)
+	mesh.aiSubdivType.set(1)
     mesh.aiSubdivIterations.set(2)
