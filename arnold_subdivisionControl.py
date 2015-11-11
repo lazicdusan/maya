048eaf189222fc89f2b6605ee85d801012a55366
lazicdusan <lazicdusan@gmail.com>
2015-11-11 21:32:53 +0100

    fixed the indentations

--- arnold_subdivisionControl.py
+++ arnold_subdivisionControl.py
@@ -7,4 +7,4 @@ for mesh in pm.listRelatives(pm.selected(),c=1,ad=1,typ='shape'):
 #select the top geo_C_001_GRUP and run this python to get all the subobjects to catclark 2:
 for mesh in pm.listRelatives(pm.selected(),c=1,ad=1,typ='shape'):
 	mesh.aiSubdivType.set(1)
-    mesh.aiSubdivIterations.set(2)
+	mesh.aiSubdivIterations.set(2)
