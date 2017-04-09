71ca2cc0ab7d36d7f85aace856c3b08c34c963af
lazicdusan <lazicdusan@gmail.com>
2017-04-09 11:08:24 +0200

    Add files via upload

---
+++ PyMel.py
@@ -0,0 +1,191 @@
+#stuff collected from various sources intended for personal reminders
+
+#make a variable for PyNode
+ASSET='*_assetName*'
+#pm PyNode 01
+CUSTOMPYNODE = pm.PyNode(ASSET)
+
+#make the empty group with the cameras
+cameraGroup = pm.group(n="custom_group", em=True)
+# create a camera and set the focal length
+renderCam = pm.createNode("camera")
+renderCam.getParent().rename("turntable_camera")
+renderCam.focalLength.set(50)
+renderCam.getParent().setParent(cameraGroup)
+renderCam.displayResolution.set(True)
+renderCam.getParent().tz.set(camDistance) #camDistance is calculated elsewhere
+
+# make a sphere and set its attributes
+matteSphGeo = pm.polySphere(n='sphere_matte',r=0.015,ch=0,cuv=1)
+pm.setAttr('sphere_matte.t',(0.162,-0.121,-0.729))
+#this resets transformations
+pm.makeIdentity(matteSphGeo,t=1,r=1,s=1,a=1)
+#makes the sphere a child of a group previously made
+pm.parent(matteSphGeo,'turntable_camera')
+
+#get the bounding box of the asset
+allGrpBoundingBox = CUSTOMPYNODE.boundingBox()
+#get cented, depth and width of the bounding box
+allGrpBoundingBox.center()
+allGrpBoundingBox.depth()
+allGrpBoundingBox.width()
+#move the asset to the center of the bounding box
+CUSTOMPYNODE.setTranslation(allGrpBoundingBox.center())
+
+# create the Light Setup
+#custom arnold directional light
+lightsGroup = pm.group(n="light_group", em=True)
+keyLight = pm.shadingNode('directionalLight',al=True,name='key_light')
+keyLight.translate.set(-5.002,4.155,0.002)
+keyLight.rotate.set(-76.186,-43.135,25.961) 
+keyLight.color.set((1,0.99,0.95))
+keyLight.aiExposure.set(2.5)
+keyLight.aiAngle.set(10.000)
+keyLight.aiShadowDensity.set(1)
+keyLight.aiVolumeSamples.set(2)
+keyLight.setParent(lightsGroup)
+
+#custom arnold skydome light
+domeLight = pm.shadingNode('aiSkyDomeLight',al=True,name='dome_light')  
+domeLight.intensity.set(1)
+domeLight.aiSamples.set(4)
+domeLight.setParent(lightsGroup)
+
+#animate the asset rotating it for 360 degrees
+CUSTOMPYNODE.setParent(animateGroup)
+animateGroup.ry.setKey(t=1001, outTangentType="linear", inTangentType="linear") 
+animateGroup.ry.setKey(t=1059 + 1, v=360, outTangentType="linear", inTangentType="linear")
+
+# create the ground plane
+ground = pm.polyPlane(w=60,h=60,name='ground_plane',ch=0)
+pm.setAttr('ground_plane.primaryVisibility',0)
+
+# look through the render cam
+try :
+    pm.mel.lookThroughModelPanel("turntable_camera", "modelPanel4")
+except : pass
+
+# lock and Hide multiple attrs
+objects=['sphereMatte','sphereShiny','sphereChrome','groundPlane','turntableCam','key_light']
+attrToHide = ['.translateX','.translateY','.translateZ','.rotateX',
+'.rotateY','.rotateZ','.scaleX','.scaleY','.scaleZ']
+
+for obj in objects :
+    for attrs in (attrToHide):     
+        cmds.setAttr(trans+attrs,keyable=0,cb=0,lock=1)
+
+# global render settings
+renderGlobals = pm.PyNode("defaultRenderGlobals")
+renderGlobals.animation.set(True)
+renderGlobals.extensionPadding.set(4)
+renderGlobals.animationRange.set(0)
+renderGlobals.startFrame.set(1)
+renderGlobals.endFrame.set(60)
+renderGlobals.putFrameBeforeExt.set(1)
+
+# set the ouput resolution
+defaultResolution = pm.PyNode("defaultResolution")
+defaultResolution.width.set(1920)
+defaultResolution.height.set(1080)
+defaultResolution.deviceAspectRatio.set(1.777777)
+
+#change render to arnold
+pm.mel.eval("setCurrentRenderer arnold")
+
+# create the default arnold node.
+mtoa.core.createOptions()
+  
+# arnold render settings
+#PyNode 02
+arnoldRenderGlobals = pm.PyNode("defaultArnoldRenderOptions")
+arnoldRenderGlobals.AASamples.set(5)
+arnoldRenderGlobals.GIDiffuseSamples.set(1)
+arnoldRenderGlobals.GIGlossySamples.set(1)
+arnoldRenderGlobals.GIRefractionSamples.set(1)
+arnoldRenderGlobals.display_gamma.set(1.0)
+arnoldRenderGlobals.light_gamma.set(2.2)
+arnoldRenderGlobals.shader_gamma.set(2.2)
+arnoldRenderGlobals.texture_gamma.set(2.2)
+      
+# set output format to exr
+defaultArnoldDriver = pm.PyNode("defaultArnoldDriver")
+defaultArnoldDriver.aiTranslator.set("exr")
+  
+# set colorSpace to Linear
+pm.setAttr('defaultViewColorManager.imageColorProfile',2)
+pm.setAttr('defaultViewColorManager.displayColorProfile',3)
+
+# assign Shaders
+# Matte Sph Shader
+matteSphSG = pm.sets(renderable=True,noSurfaceShader=True,empty=True,name='sphere_matte_sg')
+matteSph = pm.shadingNode('aiStandard',asShader=True,name='sphere_matte_shader')
+matteSph.color.set((0.5,0.5,0.5))
+matteSph.Kd.set(1)
+matteSph.diffuseRoughness.set(0.350)
+matteSph.Ks.set(0)
+matteSph.specularRoughness.set(0.467)
+matteSph.IOR.set(1.0)
+matteSph.sssRadius.set(0.1,0.1,0.1)
+pm.connectAttr('sphere_matte_shader.outColor','sphere_matte_sg.surfaceShader')          
+cmds.sets('sphere_matte',e=True, forceElement= matteSph + 'SG')
+
+#get attributes from cameras one way
+camName=pm.ls('CAM:*',type='camera',ap=True)
+fl=pm.getAttr(camName[0].fl)
+hfa=pm.getAttr(camName[0].hfa)
+vfa=pm.getAttr(camName[0].vfa)
+ncp=pm.getAttr(camName[0].ncp)
+fcp=pm.getAttr(camName[0].fcp)
+
+#get attributes the other way
+transform =  mesh.getParent()
+tx = transform.translateX.get()
+ty = transform.translateY.get()
+tz = transform.translateZ.get()
+
+rx = transform.rotateX.get()
+ry = transform.rotateY.get()
+rz = transform.rotateZ.get()
+
+sx = transform.scaleX.get()
+sy = transform.scaleY.get()
+sz = transform.scaleZ.get()
+
+#get first and last frame of the shot 
+minTime=pm.playbackOptions(q=True,minTime=True)
+maxTime=pm.playbackOptions(q=True,maxTime=True)
+
+#try this with camTrans, cam assigning of two variables 
+camTrans, cam = pm.camera()  # create a new camera
+cam.setFocalLength(100)
+fov = cam.getHorizontalFieldOfView()
+cam.dolly(-3)
+cam.track(left=10)
+cam.addBookmark('new')
+
+'''
+In PyMEL, all you have to do is type help(nt.Camera) in the python script
+editor to find out all the things a camera node can do, or just look up 
+the Camera class in the PyMEL docs.
+'''
+
+#find out if some of the multiple objects exist
+if pm.ls(["foo", "leFoo", "third"]):
+    print "at least one exists"
+
+#find out if all objects exist
+names = ["foo", "leFoo", "third"]
+found = pm.ls(names)
+if len(found) == len(names):
+    print "all objects exist"
+
+#deal with the objects which exist
+items = ["foo", "leFoo", "third"]
+for item in items:
+    if cmds.objExists(item):
+        print item, "exists"
+    else:
+        print item, "does not exist"
+
+#get the scene name
+sn = pm.sceneName()
---
+++ PythonCookbook.py
@@ -0,0 +1,253 @@
+#stuff collected from various sources intended for personal reminders
+
+#unpacking
+data = [ 'ACME', 50, 91.1, (2012, 12, 21) ]
+name, shares, price, date = data
+print (name) #prints first item
+print (shares) #prints second
+print (price)
+print (date)
+
+#unpacking with a star
+record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
+name, email, *phone_numbers = record #star is the final/rest stuff
+print (name) #prints first item
+print (email) #prints second
+print (phone_numbers) #prints the rest
+
+#star can be in the middle
+grades = (1,2,3,4,5) 
+first, *middle, last = grades #star is the middle
+print (middle) #prints the middle items only
+
+#count the most mentioned item in the list
+from collections import Counter
+words = [
+'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+'my', 'eyes', "you're", 'under'
+]
+word_counts = Counter(words) 
+top_three = word_counts.most_common(3) #gets top three results
+print(top_three)
+
+word_counts['not'] #counts how many times 'not' happens
+word_counts['eyes'] #counts how many times 'eyes' happens
+
+morewords = ['nja','sdfadsf','OOO']
+word_counts.update(morewords) #add new stuff to word_counts
+
+#list comprehensions
+
+jaPunoVolimTijanu = [1,2,3,4,5,6,7,8,9,10000000000000000]
+
+for t in jaPunoVolimTijanu:  #2
+	print (t)                  #1   
+
+COMPREHENSION = [print (t) for t in jaPunoVolimTijanu]
+
+squares = []
+for x in range(10):     #2
+	squares.append(x**2)  #1
+
+COMPREHENSION = squares = [x**2 for x in range(10)]
+
+#list comprehensions with if
+mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+
+for n in mylist:
+	if n > 0:
+		print (n)
+
+COMPREHENSION = [n for n in mylist if n > 0]
+
+for n in mylist: #2 
+  if n > 0:      #3 
+    print (n)    #1 
+
+# Determine if any .py files exist in a directory
+import os
+dirname = 'D:\Dusan\Stvaralastvo\Skripte'
+files = os.listdir(dirname)
+
+if any(name.endswith('.py') for name in files): #endswith and startswith can be used to check stuff like this
+	print('There be python!')
+else:
+	print('Sorry, no python.')
+
+#str.find(), str.endswith(), str.startswith()
+#startswith basic filtering
+choices = ('http:', 'https:', 'ftp:')
+url = 'http://www.python.org'
+if url.startswith(choices):
+	print ('yes sir')
+
+text = 'yeah, but no, but yeah, but no, but yeah'
+# Exact match
+text == 'yeah'
+
+# Match at start or end
+text.startswith('yeah')
+
+text.endswith('no')
+
+# Search for the location of the first occurrence
+text.find('no')
+
+# search and replace
+text.replace('yeah', 'yep')
+
+#dictionaries reminder
+c = {'y': 2, 'z': 4 }
+print (len(c))
+print (list(c.keys()))
+print (list(c.values()))
+
+#combining strings with join
+parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+' '.join(parts) #'Is Chicago Not Chicago?'
+','.join(parts) #'Is,Chicago,Not,Chicago?'
+''.join(parts) #'IsChicagoNotChicago?'
+
+data = ['ACME', 50, 91.1]
+','.join(str(d) for d in data) #'ACME,50,91.1'
+
+#string combining and printing
+print(a + ':' + b + ':' + c) # Ugly
+print(':'.join([a, b, c])) # Still ugly
+print(a, b, c, sep=':') # Better
+
+#getting a random number value from a list
+import random
+values = [1, 2, 3, 4, 5, 6]
+print (random.choice(values))
+
+#decorator
+def verbose(original_function): 
+    # make a new function that prints a message when original_function starts and finishes
+    def new_function(*args, **kwargs):
+        print("Entering decorator like a baws", original_function.__name__)
+        original_function(*args, **kwargs)
+        print("Exiting decorator like a baws", original_function.__name__)
+ 
+    return new_function
+
+@verbose
+def widget_func():
+    # some code
+    print ('this is going to work')
+
+widget_func()
+
+#make a class
+class Employee:
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+
+   def displayEmployee(self):
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+
+#This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+#This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+
+emp1.displayEmployee()
+emp2.displayEmployee()
+
+#make a second class
+class Namirnice:
+    def __init__(self, ime, podgrupa, cena, rokTrajanja):
+        self.ime = ime
+        self.podgrupa = podgrupa
+        self.cena = cena
+        self.rokTrajanja = rokTrajanja
+    #create the first function in the class
+    def printajNamirnice(self):
+        print ("Ime namirnice je", self.ime,"iz podgrupe", self.podgrupa, "," , "cena je:", self.cena, "evra")
+
+    #crete the second function
+    def printajRokoveTrajanja(self):
+        print ("Rok trajanja za", self.ime, "je do", self.rokTrajanja)
+
+
+#This would create first object the class"
+namirnica01 = Namirnice("kupus", "povrce", 5, 2017)
+#This would create second object of the class"
+namirnica02 = Namirnice("mleko", "mlecni proizvod", 2, 2016)
+
+#call the first function from the class
+namirnica01.printajNamirnice()
+#call the second function from the class
+namirnica02.printajRokoveTrajanja()
+
+#call both
+namirnica01.printajNamirnice()
+namirnica01.printajRokoveTrajanja()
+
+#make a third class
+#create a baseclass
+class Animal(object):
+  name = None
+
+  def speak(self):
+    print ("My name is ", self.name)
+
+  def walk(self):
+    print ("I don't know how to walk")
+
+  def breathe(self):
+    print ("I don't know how to breathe")
+
+#create a subclass
+class Dog(Animal):
+  breed = None
+
+  def speak(self):
+    print ("Woof!!!")
+
+  def walk(self):
+    print ("I walk on 4 legs")
+
+  #extend the functionality of the baseclass
+  def what_is_breed(self):
+    print (self.breed)
+
+class Fish(Animal):
+
+  #override the baseclass function
+  def breathe(self):
+    print ("I breathe underwater")
+
+#test how the class works
+fido = Dog()
+fido.breed = "Doberman"
+fido.what_is_breed()
+
+dory = Fish()
+dory.breathe()
+
+#print all permutations or combinations in a list
+from itertools import permutations
+items = ['a', 'b', 'c']
+#print only two
+for p in permutations(items,2):
+  print(p)
+
+#write to the external file 
+filename = "C:\\Users\\lazicdusan\\Desktop\\testFile.txt"
+txt = open(filename, "a")
+txt.write('this is written by python \n') #if \n doesn't work use os.linesep
+txt.close()
+
+#stop the cmd to not go away fast
+input('da li volis Tijanu?')
+
+#string formatting
+print ("Hello my name is %s and i am %s" %('Dusan','30'))
+print ("Hello my name is {} and i am {}".format('Dusan','30'))
+print ("Hello my name is {0} and i am {1}".format('Dusan','30'))
+print ("Hello my name is {user} and i am {age}".format(user='Dusan',age='30'))
